67ae2bf1780d95cb537913c500f8cb4ecda37103
Manyonge <arturias2019@gmail.com>
2022-06-07 07:17:22 +0300

    project initial setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import Home from './Routes/Home'
+import Credits from './Routes/Credits/Credits'
+import Customers from './Routes/Customers/Customers'
+import Debits from './Routes/Debits/Debits'
+import Stocks from './Routes/Stocks/Stocks'
 function App() {
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+
     </div>
   );
 }
---
+++ src/Routes/Debits/Debits.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Debits() {
+  return (
+    <div>Debits</div>
+  )
+}
