bccaedcece1351dd1ce1580c7e12ac1814832b4d
Manyonge <arturias2019@gmail.com>
2022-06-07 07:43:39 +0300

    Create create page for mui tuts

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import Home from './Routes/Home'
+import Home from './Home'
 import Credits from './Routes/Credits/Credits'
 import Customers from './Routes/Customers/Customers'
 import Debits from './Routes/Debits/Debits'
 import Stocks from './Routes/Stocks/Stocks'
+import Create from './Create'
 function App() {
 
 
   return (
     <div className="App">
-      
-
+      <Create />
     </div>
   );
 }
