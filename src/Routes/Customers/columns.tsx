c1a4894a306a35b8b856b4b5d0f021b907f47987
Manyonge <arturias2019@gmail.com>
2022-06-19 11:06:50 +0300

    heading over to new branch to add homepage shorcuts

---
+++ src/Routes/Customers/columns.tsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line import/prefer-default-export
+export const COLUMNS = [
+  {
+    Header: 'Instagram Handle',
+    accessor: 'instagramHandle',
+  },
+  {
+    Header: 'Phone Number',
+    accessor: 'phoneNumber',
+  },
+  {
+    Header: 'Shoe Size',
+    accessor: 'shoeSize',
+  },
+  {
+    Header: 'Hoodie Size',
+    accessor: 'hoodieSize',
+  },
+];
